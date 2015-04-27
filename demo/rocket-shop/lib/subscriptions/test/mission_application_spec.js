c4b36efb0a87ffce667981925575a5d2fba18bb8
Alex Krez <aleksandr.kr.26@googlemail.com>
2015-04-27 14:10:29 +0200

    membership application added and  membership test changed.

--- demo/rocket-shop/lib/subscriptions/test/mission_application_spec.js
+++ demo/rocket-shop/lib/subscriptions/test/mission_application_spec.js
@@ -1,8 +1,40 @@
 var assert = require('assert');
+var MembershipApplication = require('../models/membership_application');
 
-describe('New user signup', function () {
-    describe('Using valid email, first, last', function () {
-        it('let\'s them in');
+
+describe('Membership application requirements', function () {
+    var validApp;
+    before(function () {
+        //arrange the data here
+        validApp = new MembershipApplication({
+            first: 'Test',
+            last: 'User',
+            email: 'test@test.com',
+            age: 30,
+            height: 66,
+            weight: 180
+        });
+    });
+
+    describe('Application valid if...', function () {
+        it('all validators successful', function () {
+            assert(validApp.isValid(), 'Not valid');
+        });
+        it('email is 4 or more chars and contains an @', function () {
+            assert(validApp.emailIsValid());
+        });
+        it('height is between 60 and 75 inches', function () {
+            assert(validApp.heightIsValid());
+        });
+        it('age is between 15 and 100', function () {
+            assert(validApp.ageIsValid());
+        });
+        it('weight is between 100 and 300', function () {
+            assert(validApp.weightIsValid());
+        });
+        it('first and last name are provided', function () {
+            assert(validApp.nameIsValid());
+        });
     });
 });
 
