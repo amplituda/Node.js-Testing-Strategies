b5ff83bbbb0a525dce27a4623a410b77e530fa36
Alex Krez <aleksandr.kr.26@googlemail.com>
2015-04-27 11:57:00 +0200

    First test completed

---
+++ demo/rocket-shop/test/first_spec.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('A feature', function () {
+    describe('A scenario', function () {
+        it('dose something expected', function () {
+            assert(true);
+        });
+    });
+});
